cbe6ec06e3b7bf77ec90a5d9561d2dca46bd362c
Richard ZZY <andarkge@hotmail.com>
2018-07-06 21:13:00 -0700

    Initial checkin

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=riri-personal-assistant-Web-Deploy.pubxml -p:Password=9tvYT8vmcw8AKvYlftD8ABsapsN9ohSvSDRPABLuPzofPYwAZ79bpbquX7xD
+
